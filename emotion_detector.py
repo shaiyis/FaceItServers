afe768ba6adc26c6239c9d44c4e2058810fa9226
shaiyis <56585128+shaiyis@users.noreply.github.com>
2021-05-04 18:34:06 +0300

    real time messages are arriving correctly

--- emotion_detector.py
+++ emotion_detector.py
@@ -14,46 +14,19 @@ class EmotionDetector(pr.Processor):
         self.draw = pr.DrawBoxes2D(self.classify.class_names)
 
     def call(self, image):
+        predictions = []
         boxes2D = self.detect(image)['boxes2D']
         cropped_images = self.crop(image, boxes2D)
         for cropped_image, box2D in zip(cropped_images, boxes2D):
             box2D.class_name = self.classify(cropped_image)['class_name']
-            print(box2D.class_name)
-        return self.draw(image, boxes2D)
+            predictions.append(box2D.class_name)
+            # print(box2D.class_name)
+        if len(predictions) > 0:
+            return predictions[0]
+        # got more than 1 face?
+        # return self.draw(image, boxes2D)
 
-
-detect = EmotionDetector()
-# you can now apply it to an image (numpy array)
-image = cv2.imread("images/637546382815275184.jpg")
-detect.call(image)
-# predictions = detect(np.array(image))
-# print(predictions)
-
-
-# localIP = ''
-# localPort = 20001
-# bufferSize = 1024
-#
-# msgFromServer = "prediction:"
-# bytesToSend = str.encode(msgFromServer)
-#
-# UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# UDPServerSocket.bind((localIP, localPort))
-#
-# print("UDP ML server up and listening")
-#
-# while True:
-#     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-#
-#     message = bytesAddressPair[0]
-#     address = bytesAddressPair[1]
-#
-#     clientMsg = "Message from Client:{}".format(message)
-#     clientIP = "Client IP Address:{}".format(address)
-#
-#     print(clientMsg)
-#     print(clientIP)
-#
-#     # Sending the prediction to client
-#     UDPServerSocket.sendto(bytesToSend, address)
+    # detect = EmotionDetector()
+    # image = cv2.imread("images/637546382815275184.jpg")
+    # detect.call(image)
 
--- server.py
+++ server.py
@@ -1,37 +1,57 @@
-# ספרינטים בג'ירה
-# הרשאות
-# US נכונים בג'ירה
-
 import socket
+import numpy as np
+import cv2
+from emotion_detector import EmotionDetector
 
 localIP = ''
-localPort = 20001
-bufferSize = 1024
+localPort = 5402
+bufferSize = 1000000
 
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
+# msgFromServer = "Hello UDP Client"
+# bytesToSend = str.encode(msgFromServer)
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
+detect = EmotionDetector()
 
 while True:
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
     message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
+    address = bytesAddressPair[1]  # Client IP
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP Address:{}".format(address)
+    get_time = False
+    name_bytes = b""
+    time_bytes = b""
+    inx = 0
 
-    print(clientMsg)
-    print(clientIP)
+    for char in message:
+        inx += 1
+        c = char
+        # \n
+        if c == 10:
+            if get_time is True:
+                break
+            get_time = True
+            continue
+        if get_time is False:
+            name_bytes += bytes([c])
+        else:
+            time_bytes += bytes([c])
 
-    # send to ML model
-    # UDPServerSocket.sendto(bytesToSend, address)
-    # get prediction from ML model
-    # bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    img_bytes = message[inx:]
+    name = name_bytes.decode()
+    time = time_bytes.decode()
 
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+    img_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
+
+    prediction = detect.call(img_np)
+    if prediction is not None:
+        print(prediction)
+        # Sending a reply to client
+        UDPServerSocket.sendto(str.encode(prediction), address)
+
+
